fc255f67b7850d9c30129cef9d0f1c304f8d70b9
Santam Sarkar <santamsarkar@gmail.com>
2026-08-21 16:47:44 +0530

    pattern 13

---
+++ Striver's Pattern/13_v.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int rows;
+    cout<<"rows:";cin>>rows;
+    int count=1;
+    for(int i=0;i<rows;i++){
+        for(int j =0;j<=i;j++){
+            cout<< count<<" ";
+            count++;
+        }
+        cout<<"\n";
+    }
+}
