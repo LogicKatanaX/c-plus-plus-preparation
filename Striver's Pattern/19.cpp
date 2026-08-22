d0bf154aa047ff72893b45a607becc7dee971909
Santam Sarkar <santamsarkar@gmail.com>
2026-08-22 13:12:18 +0530

    Pattern 19

---
+++ Striver's Pattern/19.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int rows;
+    cout<<"Number:";cin>>rows;
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<rows-i;j++){
+            cout<<"* ";
+        }
+        for(int j=1;j<2*i+1;j++){
+            cout<<"  ";
+        }
+        for(int j=0;j<rows-i;j++){
+            cout<<"* ";
+        }
+        cout<<"\n";
+    }
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<=i;j++){
+            cout<<"* ";
+        }
+        for(int j=2;j<2*rows-2*i;j++){
+            cout<<"  ";
+        }
+        for(int j=0;j<=i;j++){
+            cout<<"* ";
+        }
+        cout<<"\n";
+    }
+    
+
+}
