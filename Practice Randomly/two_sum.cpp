6026d8d3a2f573de21abea4918d31be547ac9184
Santam Sarkar <santamsarkar@gmail.com>
2026-08-23 19:48:32 +0530

    two sum bruteforce

---
+++ Practice Randomly/two_sum.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a[50];
+    int size;
+    cout<<"Size:";cin>>size;
+    cout<<"Enter Element:";
+    for (int i=0;i<size;i++){
+        cin>>a[i];
+    }
+    int element;
+    cout<<"Element:";cin>>element;
+    for(int i=0;i<size;i++){
+        for(int j=i+1;j<size;j++){
+            if((a[i]+a[j])==element){
+                cout<<"["<<i<<","<<j<<"]";
+                cout<<"\n";
+            }
+        }
+    }
+}
