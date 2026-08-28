18820ed2502936a47f6648f7c2f12e59bff7d6a7
Santam Sarkar <santamsarkar@gmail.com>
2026-08-28 11:47:51 +0530

    Hash

---
+++ Hashing/hash1.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int>a;
+    int n,temp;
+    cout<<"N:";cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>temp;
+        a.emplace_back(temp);
+    }
+    int hash[13]={0};
+    for(int i=0;i<n;i++){
+        hash[a[i]]+=1;
+    }
+    int number;
+    for(int i=0;i<n;i++){
+        cout<<"Enter number";cin>>number;
+        cout<<hash[number]<<endl;
+    }
+
+}
---
+++ Jump_to_leetcode/frequency_of_elements.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+// int maxFrequency(vector<int>& nums, int k) {
+        
+
+// }
+
+int main(){
+    int size;
+    int temp;
+    int k;
+    cout<<"Size=";cin>>size;
+    vector<int> nums;
+    cout <<"Enter the elements:";
+    for(int i=0;i<size;i++){
+        cin>>temp;
+        nums.emplace_back(temp);
+    }
+    cout <<"Enter the K:";
+    cin>>k;
+
+
+}
