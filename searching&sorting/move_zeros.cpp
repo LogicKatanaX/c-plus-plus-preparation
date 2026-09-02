42bd0d8e14c3cfdc7eda83a2f886cadcb1115069
Santam Sarkar <santamsarkar@gmail.com>
2026-09-02 23:15:34 +0530

    Implement move zeros array solution

--- searching&sorting/move_zeros.cpp
+++ searching&sorting/move_zeros.cpp
@@ -1,24 +1,31 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
+
+int main() {
     vector<int> a;
     int size;
-    cout <<"Size:";
+    cout << "Size: ";
     cin >> size;
-    while(size--){
+
+    while (size--) {
         int temp;
         cin >> temp;
         a.emplace_back(temp);
     }
-    unordered_map<int,int> mp;
-    int target =0;
-    for (int i =0;i<a.size();i++){
-        if(mp.find(target)!=mp.end()){
-            mp[target]++;
-        }
-        mp[a[i]]=i;
-    }
-    
-    
 
+    int next_non_zero = 0;
+    for (int value : a) {
+        if (value != 0) {
+            a[next_non_zero++] = value;
+        }
+    }
+
+    while (next_non_zero < a.size()) {
+        a[next_non_zero++] = 0;
+    }
+
+    for (int value : a) {
+        cout << value << ' ';
+    }
+    cout << '\n';
 }
